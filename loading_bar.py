1e4a3315a8ecc796106b4330f74c8072dc36b64e
Makelonn <maelie.lebaron@gmail.com>
2022-07-17 22:16:34 +0200

    Loading bar class

---
+++ loading_bar.py
@@ -0,0 +1,23 @@
+import sys
+
+class LoadingBar:
+    def __init__(self, total):
+        self.total_files = total
+        self.cpt = 0
+        self.nb = 0
+        self.print_bar()
+
+    def print_bar(self):
+        self.nb = int((self.cpt*100)/self.total_files)
+        # Calculte %
+        percent = str(self.nb) + " %"
+        sys.stdout.write(("\u2588" * self.nb) + ("\u2591" * (100-self.nb)) + " " + percent)
+        sys.stdout.flush()
+        sys.stdout.write("\b" * (len(percent) + 101))
+
+    def update(self):
+        self.cpt += 1
+        self.print_bar()
+
+    def renew(self):
+        self.print_bar()
